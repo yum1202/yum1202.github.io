fa3f8c4b83e72e48b4cc76dce9b390af46ee082e
yum1202 <cjnmgirl@naver.com>
2020-09-29 21:25:59 +0900

    Add files via upload

---
+++ public/js/common.js
@@ -0,0 +1,61 @@
+$('select').each(function(){
+    var $this = $(this), numberOfOptions = $(this).children('option').length;
+  
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').not(this).each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+        $list.hide();
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+      
+
+});
+
+
+$(document).ready(function(){
+    var fileTarget = $('.filebox .upload-hidden');
+  
+      fileTarget.on('change', function(){
+          if(window.FileReader){
+              var filename = $(this)[0].files[0].name;
+          } else {
+              var filename = $(this).val().split('/').pop().split('\\').pop();
+          }
+  
+          $(this).siblings('.upload-name').val(filename);
+      });
+  }); 
